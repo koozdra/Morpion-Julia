c187b6272e5769607495a2a6c947679d27d1bb29
Dimitri Tishchenko <dtishchenko@change.org>
2017-06-26 21:20:05 -0700

    new

---
+++ new.jl
@@ -0,0 +1,787 @@
+
+
+#159 KIxBDe3YC0lF2qSz0d1y9Gth/+vburvX71v88fj9X/34
+#161 EENsjZYtNIky9IKowWvPUP97+u1tM9/7f23uz1/fz7d/g
+#162 liQbB5YJsBNoUfGwyOmNerGY43/zH3V+3+2XEm48/lrav//0
+#172 FEBMv6lokkiL84GQw1Lnd1Q9p/u7nv0/vsf513X9v+7f/rv
+
+
+#test
+#158 JElFS9IxaEKgRZaWM/eW1leNPW+sK7zdaY//sd769/H/D/g
+#160 AmErjrrIQIslAtlEnxO+UZeavHre89p7/z/n/f5f/P//w
+#160 kiQrD5kkJGqQR8bD9u9tULlv55dpa/2mHrv+9P9X//fvw
+#160 REKPh1SQWOUCMdwAlW/k0OZj2v9tV/Ou7dZee+9v3fb7v/
+#161 EENMjZYvchEYeEFoFNPbaPt9+uu7vWu8+7Ppu3d/e59vfw
+#161 AIaPnmzS8NvGibKRFVRtzl5dj71D+/3z/Pv2Pf+27X93
+#161 KQFErmOpmFlS2oBjCLaOqIt1fvmo/fzjb5//Zv77f3u3N/Q
+#162 liQbB5YJsBpaUg+HK4qc3r1Y2d5n3V2+37ZcSanc/NW1f//o
+#162 liQbB5YJsBNoUfCxkdMa9WUxxv/lfOvbd/bLie0a/rW1f//w
+#162 phQpD/h2cJABAih7m653U5vmqr9f7f/39b6d8/f57/uv
+#163 CBNnT2OJiCLhUlEl1/09mqhXPXx9useb13XP+fdvvfV7f9
+#163 AUCsv8Z6g6GZUbYOS2PG/pdM7aqHz29n6/297u9P73d/bg
+#163 EImMr5s5XQxFhOlNkW7K+vdpZuovp/33KX87evz2lvbf7u
+#164 HYiAjzKyU4Y2ZI2QtMKMrC/Cfv1fvltLn5vF3rrztz/rPf72g
+#165 0yAiD2zS2WLOpL6HZny07vmdaZv/X72z/729+v/du/7
+#166 YEKPR0zVMETGGetAsUjEYT+/+2s3v7HL3xq9yUzb2/3u+377g
+#166 IIMnTahsGFUcZm6pTKDfayUY/1q329Pu1af+fW1m/8u///u
+#167 FEBMv5nUNjXLREvmOhmKzZDdn/9+axrvyc83X1Nb//9s7/s
+#167 RSJIh84liiFRcOPTeIshl+9a/vRX9+1P8+//yr3Qefv//9g
+#167 LkwQS6WFYgV55zJV2SWokT2396L/f+r/6/787yG6+/r//Y
+#167 FEBMv5nUNjXLREvmOhmKzZDdn79/H7q/I55uvqa3//7Tv/o
+#167 FSwQi9YU0Q68E9K1zlbkTh37db+8p2ffFv/vz5trvsVvf2Tg
+#169 CBMXT2TomgiXeiKtAq1lWu397P7jbOu5hV35cvzf+/dZb+O3w
+#169 FxwYD6lkowlgzpULRk+nnmTa5/93rni/fG+3jd9t/+rHvHv4
+#169 AYOOj2zS8MbVkjsktRYym3Q57339NVv9LnO7Jn37f97zf/b
+#169 CCkVT/SQlERFkeOlN2xzljsn7Vzf6/tN+rveb/V3e752/e+g
+#169 IiAjf1nSmo7NUI3kulmIm2G7Pv79Vdf5nPPTmnf9f93N/+2
+#169 7EBET5ZlVgRpAJW6RkR43b4oGXe/2/1lz9/t453v6//if3/u
+
+#167 FEBMv5nUNjXLRGuLDQnHZpu7V39v6Pt6Z7zy9uvpN/7kv/2
+#170 FEBMv6lokkqOS7M5RPJOs/2+zl/cc78s8b4+O721a/vUn/98
+#170 FEFMq7grY1JGooKxrhlfxd66n/79d0+vMmeNU/Vbb+9fr99bQ
+#170 AYOOj14TKmgrRKIBiYK9fprP2W39f3HnnZX+/HPt7/dam/v54
+#171 IiAjf2jokjJLE6eTC+ptL2h36ev3h9+VMuurrpfS+/31U/nv8
+#172 FEBMv6lokkiL84GQw1Lnd1Q9p/u7nv0/vsf513X9v+7f/rv
+#172 KBEkq8OtZI0FslQMUpFSXG3ertmr/rG/N95an7q5um/f3/7+08
+#174 FxwYD6lkowlD6GwRqS1m75Sep/nf9yW/7uX1ur3f++0nft+/g
+
+#175 IiAjf2jokjBL84KQJYhayE/XD7m3bd/1i/b+Jx59e1/vbl7+7f4
+#175 IiAjf2jokjBL84KQJYhazH1jvu2d3fap19v4nHn17X+9unf7t/g
+#175 CBMXT2TomgiXeiKNAlJlspen5+17309Zfeu1iZ712/3u47/u3+
+#175 CBMXT2TomgiXeiKNAlJFlP64f512vfVuN926nHPddv97uXf7t/g
+#175 FxwYD6lkowlD6GwRKSGs5T9kf5n+720dv2+mkefu1v++1Pfb9/
+#175 FEBMv6lokkqKC/4YXowwPzGP31vu+bHr73jpHn69b/vbTv72/w
+#175 CBMXT2TomgiXeiKNAlJFlKvy8dtPtd+txvu3U457rt/vd07/dv8
+#175 AYOOj1VpKGGimNoZQioUyn+cX887Pfz1r7ttOOe7bf73cu/vb/
+
+#177 CBMXT2TomgiXeiKtAq0kv1ta86fr7HX4xZ1ndct9X/93l9/9++
+#177 FEBMv6lwkkqKSwcwzBkf1XzetN6P3Ht8zeuu5rvqv/9uPv/374
+#177 FEBMv6lokkiL84GQw9LldSufvv7lV+u+I3vm3fm3/85v3/tv2
+#177 FEBMv6lokkqKSwcwzBgenXP9/XZzevWnx8uu5rvqvf+9L7/798
+
+#178 AYOOj1VpKGGimNoZQioUyn+cf2Hn59/PFrXeUOM92zf/3ce//vvo
+
+# new one, TODO visualize
+#178 kiQjjq5SCREWSWmi1kYvbXp0r9Vzn++z308h3TdRm/X93l3/6fLW
+#171 KRFEqzHWskaC2VFDFWmIkl1fbu/JnC+N582f5vf09Xndv/v7c8
+
+# really active in the 160's
+#169 0yAij1VlSSRWksIzgzcdXa3f7fri/K+0HbvlWf6/v+8Uf59+
+
+
+
+
+#extremely intermitent error stepping back from this one (could be out of bounds of the board)
+#156 JxQqqzouUBRGkNAe62tsbczWf/Gru9/3Pzm6vF5p293/0
+#ERROR: BoundsError()
+# in validate_line at /home/dimitri/Dropbox/julia/morpion.jl:239
+# in make_move at /home/dimitri/Dropbox/julia/morpion.jl:566
+# in random_completion at /home/dimitri/Dropbox/julia/morpion.jl:156
+# in end_search at /home/dimitri/Dropbox/julia/morpion.jl:815
+# in anonymous at no file:580
+# in include at ./boot.jl:245
+# in include_from_node1 at loading.jl:128
+# in process_options at ./client.jl:285
+# in _start at ./client.jl:354
+# in _start_3B_1723 at /usr/bin/../lib/x86_64-linux-gnu/julia/sys.so
+#while loading /home/dimitri/Dropbox/julia/searcher.jl, in expression starting on line 260
+
+#ERROR: BoundsError()
+# in validate_line at C:\dimitri\Morpion-Julia\morpion.jl:251 (repeats 2 times)
+#while loading C:\dimitri\Morpion-Julia\runner.jl, in expression starting on line
+# 314
+
+# ERROR: BoundsError()
+#  in validate_line at C:\dimitri\Morpion-Julia\morpion.jl:250
+#  in make_move at C:\dimitri\Morpion-Julia\morpion.jl:559
+#  in random_completion at C:\dimitri\Morpion-Julia\morpion.jl:170
+#  in end_search at C:\dimitri\Morpion-Julia\morpion.jl:781
+#  in anonymous at no file:599
+#  in include at boot.jl:245
+#  in include_from_node1 at loading.jl:128
+#  in process_options at client.jl:285
+#  in _start at client.jl:354
+# while loading C:\dimitri\Morpion-Julia\runner.jl, in expression starting on line
+#  312
+
+
+import Base.hash
+import Base.copy
+include("morpion.jl")
+
+
+filename = string(rand())[3:end]
+
+if length(ARGS) == 1
+	filename = ARGS[1]
+end
+
+isrestarting = isfile("$(filename).library")
+
+type Searchy
+	points_hash::Uint64
+	score::Int32
+	pack::String
+	visits::Int32
+	step_created::Uint32
+	step_visited::Uint32
+end
+
+function Searchy(morpion::Morpion)
+	Searchy(points_hash(morpion),score(morpion),generate_pack(morpion),0,0,0)
+end
+
+function isless(a::Searchy, b::Searchy)
+	a.score < b.score
+end
+
+# idea: write complete state to file_path
+## index
+## end_searched
+## cache (maybe)
+
+function sync (index, searched, min_accept, name, load_visits)
+
+	println("SYNC")
+
+	file_path = "./data/"
+	file_name = "$(file_path)$(name).library"
+
+	block_counter = 1
+	while isfile("$(file_name).lock")
+		println("LIBRARY LOCK $(block_counter) waiting three seconds...")
+		sleep(3)
+		block_counter += 1
+	end
+
+	touch("$(file_name).lock")
+
+	touch("$(file_name)")
+
+	open("$(file_name)","r") do file
+		i = 0
+		for line in eachline(file)
+			file_searchy = eval(parse(line))
+
+			if !load_visits
+				file_searchy.visits = 0
+			end
+
+			#end_searched[file_searchy.points_hash] = true
+
+			if !haskey(index, file_searchy.points_hash) && !haskey(searched, file_searchy.points_hash) && file_searchy.score >= min_accept
+				#println(file_searchy.score)
+				#index[file_searchy.points_hash] = file_searchy
+			end
+
+			i += 1
+		end
+
+		close(file)
+	end
+
+	open("$(file_name)","w") do file
+		truncate(file,0)
+		for index_searchy in values(index)
+			if index_searchy.score >= min_accept
+				write (file, "$(index_searchy)\n")
+			end
+		end
+		close(file)
+	end
+
+	rm("$(file_name).lock")
+end
+
+
+step = 0
+max_score = 0
+max_pack = ""
+
+searchy = Searchy(random_morpion())
+
+#searchy = Searchy(unpack_pack("0yAij1VlSSRWksIzgzcdXa3f7fri/K+0HbvlWf6/v+8Uf59+"))
+
+#benchmark
+#searchy = Searchy(unpack_pack("JhRdb1OMayBDnrJbM8Upr3fetf+/79ug"))
+# 162 liQbB5YJsBpaUg+DNSsa+WUxxv/lfOubfvtlyWp3PzVtX//8
+# 162 liQbB5YJsBNoUfGwyOmNe2Uxxv/mPur61/bLiWp3Py1tX//6
+# 162 liQbB5YZqAyWog+DNxU5e16sbO8z7V97d9suJdo1/NW1f//o
+# 162 kiQbB5YJsLJaUVHL6Hg18spnG/+Y+6vbf7ZaS1O5+tbV//+g
+
+
+
+# peak? benchmark
+#154 CiEjb7GMYJSZZXBOXdWvZ/VYfWe/95V5f+vK9/zbX/u
+#searchy = Searchy(unpack_pack("CiEjb7GMYJSZZXBOXdWvZ/VYfWe/95V5f+vK9/zbX/u"))
+
+#searchy = Searchy(unpack_pack("AYOOj1VpKGGimNoZQioUyn+cf2Hn59/PFrXeUOM92zf/3ce//vvo"))
+#searchy = Searchy(unpack_pack("7mBHf7A"))
+#searchy = Searchy(unpack_pack("7EBET5ZlVgRpAJW6RkR43b4oGXe/2/1lz9/t453v6//if3/u"))
+# p = unpack_pack("kiQjjq5SCREWSWmi1kYvbXp0r9Vzn++z308h3TdRm/X93l3/6fLW")
+# println(score(p))
+# random_completion(morpion_evaluator(p))
+#
+# println(score(p))
+
+# p = unpack_pack("kiQjjq5SCREWSWmi1kYvbXp0r9Vzn++z308h3TdRm/X93l3/6fLW")
+# e = morpion_evaluator(p)
+# for i = 1:25
+# 	remove_loose_moves(e);
+# end
+# println(score(p))
+# println(score(e.morpion))
+# random_completion(e)
+# println(score(e.morpion))
+#
+# searchy = Searchy(e.morpion)
+
+
+index = Dict{Uint64,Searchy}()
+index[searchy.points_hash] = searchy
+
+
+# prime
+
+# f = 0
+# i = 0
+# tic()
+# for i in 1:4000
+# 	searchy = Searchy(random_morpion())
+# 	if searchy.score >= f
+# 	  f = searchy.score
+# 		println("$(i). $(f)")
+# 		index[searchy.points_hash] = searchy
+# 	end
+# 	i+=1
+# end
+# toc()
+
+
+searched = Dict{Uint64,Searchy}()
+end_searched = Dict{Uint64,Bool}()
+
+morpion_cache = Dict{Uint64,Morpion}()
+
+function morpion_from_hash(morpion_cache::Dict{Uint64,Morpion}, searchy::Searchy)
+	if haskey(morpion_cache, searchy.points_hash)
+		return morpion_cache[searchy.points_hash]
+	end
+
+	morpion_cache[searchy.points_hash] = unpack_pack(searchy.pack)
+end
+
+function modification(morpion, visits)
+	morpion_dna = generate_dna(morpion)
+
+	position = ((visits) % score(morpion)) + 1
+	morpion_dna[dna_index(morpion.moves[position])]	= -1
+
+	eval_dna(morpion_dna)
+end
+
+function modification_triple(morpion, visits)
+	morpion_dna = generate_dna(morpion)
+
+	for i = 1:3
+		morpion_dna[dna_index(morpion.moves[rand(1:end)])]	= -1
+	end
+
+	eval_dna(morpion_dna)
+end
+
+#parameters
+timeout_score_multiplier = 10
+
+# -5 records
+index_accept = -5
+discover_reset = -5
+
+end_search_accept = -20
+end_search_trials = 40
+
+focus_modifier = 1/2000
+focus_min = focus_modifier
+focus_inactive_reset = 10000
+focus = focus_min
+# the minimum score multiplier number to get to before resetting focus
+focus_min_reset = 1
+
+
+inactivity_reset = 5000
+
+last_active_step = 0
+
+end_search_activated = true
+
+end_search_interval = 500
+
+
+auto_end_search = -4
+
+min_max_index_length = 100
+max_index_length = min_max_index_length
+index_length_increment = 100
+forget_percent_save = 0.5
+
+max_index_score = 0
+
+sync_interval = 3000
+sync_min = 10000
+
+min_visited_score = 0
+
+discovery_count = 0
+# tuned >4, <16
+# 20 was amazing, trying something lower
+discovery_interval = 10
+
+min_score_end_search = 105
+
+max_score_discover_multiplier = 0
+
+tic()
+
+while true
+
+	is_new_found = false
+	is_record = false
+	is_improvement = false
+	is_new_equal = false
+	is_max_score_found = false
+
+
+	#selection
+	#searchy = pool[(step%length(pool)) + 1]
+
+	# 1,4 really good
+	# 1,2 really good in random modification (150 all around same evening)
+	function exploreBoundReducer (a,b)
+
+		# 0.33 amazing here
+		#t = 0.33
+		t = 0.6
+		bound_mult = 0.6
+
+		sa = a.score-(a.visits/(a.score*t))
+		sb = b.score-(b.visits/(b.score*t))
+
+		if (a.visits > a.score * bound_mult)
+			sa = 0
+		end
+
+		if (b.visits > b.score * bound_mult)
+			sb = 0
+		end
+
+		if sa > sb || sa == sb
+			return a
+		else
+			return b
+		end
+	end
+
+	function exploreScoreReducer(a, b)
+		t = 10
+
+		sa = a.score-(a.visits/t)
+		sb = b.score-(b.visits/t)
+
+		if sa > sb || sa == sb #&& randbool()
+			return a
+		else
+			return b
+		end
+	end
+
+	function exploreMinReducer(a, b)
+		min = 2
+		t = 0.33
+
+		sa = a.score-(a.visits/(a.score*t))
+		sb = b.score-(b.visits/(b.score*t))
+
+		if (a.visits < min)
+			sa = 1000
+		end
+
+		if (b.visits < min)
+			sb = 1000
+		end
+
+		if sa > sb || sa == sb
+			return a
+		else
+			return b
+		end
+	end
+
+	function focusReducer(a, b)
+		t = focus
+
+		sa = a.score-(a.visits/(a.score*t))
+		sb = b.score-(b.visits/(b.score*t))
+		if sa > sb || sa == sb #&& randbool()
+			return a
+		else
+			return b
+		end
+	end
+
+	function rank(searchy, t)
+		return searchy.score - (searchy.visits / (searchy.score * t))
+	end
+
+	function exploitReducer (a,b)
+		# records 1
+		t = 1
+
+		ra = rank(a, t)
+		rb = rank(b, t)
+
+		if ra == rb
+			if a.step_visited < b.step_visited
+				return a
+			end
+
+			return b
+		end
+
+		if ra < rb
+			return b
+		end
+
+		return a
+	end
+
+	function exploreReducer(a, b)
+		# 0.33 records
+		t = 0.33
+
+		ra = rank(a, t)
+		rb = rank(b, t)
+
+		if ra == rb
+			if a.step_visited < b.step_visited
+				return a
+			end
+
+			return b
+		end
+
+		if ra < rb
+			return b
+		end
+
+		return a
+	end
+
+	#searchy = reduce ( exploitReducer , values(index))
+	# if step & 1 == 0
+	# 	searchy = reduce (exploitReducer, values(index))
+	# else
+	# 	searchy = reduce (exploreBoundReducer, values(index))
+	# end
+
+	if step & 1 == 0
+	 	searchy = reduce (exploitReducer, values(index))
+	else
+		searchy = reduce (exploreReducer, values(index))
+	end
+
+	#searchy = reduce ( exploit , values(index))
+
+	searchy.step_visited = step
+
+	morpion = morpion_from_hash(morpion_cache, searchy)
+
+	min_visited_score = min(min_visited_score, searchy.score)
+
+	if score(morpion) > max_index_score
+		max_index_score = score(morpion)
+	end
+
+
+	if score(morpion) > max_score
+		max_score = score(morpion)
+		max_pack = generate_pack(morpion)
+
+		println()
+		println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+		println("$(step). $(max_score) $(max_pack)")
+		println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+		println()
+
+		max_index_score = max_score
+
+		min_visited_score = max_score
+
+		# experimental
+		max_index_length = min_max_index_length
+
+		empty!(end_searched)
+	end
+
+	#eval_morpion = modification(morpion, searchy.visits)
+	#if step & 1 == 0
+	#	eval_morpion = modification_triple(morpion, searchy.visits)
+	#else
+	#	#eval_morpion = modification(morpion, searchy.visits + floor(searchy.score * offset))
+	#	eval_morpion = modification(morpion, searchy.visits)
+	#end
+
+	#eval_morpion = modification_triple(morpion, searchy.visits)
+	if searchy.visits & 1 == 0
+		eval_morpion = modification(morpion, searchy.visits / 2)
+	else
+		eval_morpion = modification_triple(morpion, searchy.visits)
+	end
+
+	# if searchy.visits <= searchy.score * 2
+	# 	eval_morpion = modification_triple(morpion, searchy.visits)
+	# else
+		eval_morpion = modification(morpion, searchy.visits)
+	# end
+
+
+	eval_hash = points_hash(eval_morpion)
+
+	if haskey(morpion_cache, eval_hash)
+		morpion_cache[eval_hash] = eval_morpion
+	end
+
+	# dimitri
+	# if step % 100 == 0
+	# 	println("$step. $(score(morpion)) $(searchy.visits) $(round(searchy.visits / score(morpion), 2))")
+	# end
+
+	#min_accept = score(morpion) + index_accept
+	#min_accept = pool_score + (index_accept * 2)
+	min_accept = min(score(morpion) + index_accept, min_visited_score)
+	#min_accept = min_visited_score
+
+	#println("$step. $(score(morpion)) $(searchy.visits) $(min_visited_score)/$(max_index_score)/$(max_score) $(length(index)),$(length(morpion_cache))))")
+
+	if score(eval_morpion) >= min_accept && !haskey(index,eval_hash) && !haskey(searched,eval_hash)
+
+		if score(eval_morpion) > max_score
+			#empty!(index)
+			#filter!( (k,v) -> v.score >= score(eval_morpion) + index_accept , index)
+		end
+
+		indicator = "->"
+		gap = " "
+
+		if score(eval_morpion) >= score(morpion)
+			indicator = "=>"
+			gap = ""
+			is_new_equal = true
+		end
+
+		if score(eval_morpion) > score(morpion)
+			indicator = "==>"
+			gap = ""
+
+			is_improvement = true
+		end
+
+		is_new_found = true
+
+
+
+		discovery_count += 1
+
+		#focus += focus_modifier
+
+
+		max_score_discover_multiplier = max(max_score_discover_multiplier, convert(Int, floor(searchy.visits/score(morpion))))
+
+		# main println
+		println("$step. $gap$(score(morpion)) $(indicator) $(score(eval_morpion)) $(searchy.visits) $(min_visited_score)/$(max_index_score)/$(max_score) ($(length(index)),$(length(morpion_cache)),$(length(end_searched)), $(max_index_length), $(max_score_discover_multiplier), $(step - last_active_step), $(round(focus, 2)) ")
+
+
+
+
+
+		#if score(eval_morpion) >= score(morpion) + discover_reset && searchy.visits > score(morpion)
+		if score(eval_morpion) >= score(morpion) + discover_reset
+		#if score(eval_morpion) >= min_visited_score
+			searchy.visits = 0
+			#searchy.visits = max(0, searchy.visits - 1)
+			#searchy.visits = max(0, searchy.visits - (searchy.visits % ))
+
+		end
+
+
+		#new_found
+		new_searchy = Searchy(eval_morpion)
+
+		new_searchy.step_created = step
+
+		#verify(eval_morpion)
+
+
+
+		index[eval_hash] = new_searchy
+
+	end
+
+	if score(eval_morpion) > score(morpion)
+		max_index_score, i = findmax(map((x)->x.score, values(index)))
+	end
+
+	if score(eval_morpion) > max_score
+
+		is_record = true
+
+	end
+
+	if searchy.visits > searchy.score * timeout_score_multiplier
+
+		println("$(step). COMPLETE $(searchy.score) ($(searchy.visits))  ($(length(index)-1))")
+
+
+		delete!(index, searchy.points_hash)
+
+		searched[searchy.points_hash] = searchy
+
+
+		max_index_score, i = findmax(map((x)->x.score, values(index)))
+
+
+		#empty!(pool)
+	end
+
+
+
+	#if step % 10000 == 0
+	#	empty!(morpion_cache)
+	#end
+
+	if step % 1000 == 0
+		time = toq()
+		println("$(step). $(max_score) $(max_pack) ($(max_index_length), $(step - last_active_step), $(round(focus, 2)))")
+		println(" elapsed: $(time)")
+		tic()
+	end
+
+	if max_score >= min_score_end_search &&
+		end_search_activated &&
+		!haskey(end_searched, searchy.points_hash) #&&
+		#searchy.visits > searchy.score * 0.2
+
+		discovery_count = 0
+
+		# function end_search_reduce(a,b)
+		# 	(haskey(end_searched, a.points_hash) ? 0 : 1, a.score, -a.visits) >
+		# 	(haskey(end_searched, b.points_hash) ? 0 : 1, b.score, -b.visits) ? a : b
+		# end
+		# end_searchy = reduce(end_search_reduce, values(index))
+
+		end_searchy = searchy
+
+
+		end_morpion = morpion_from_hash(morpion_cache, end_searchy)
+
+		println ("end search: $(score(end_morpion))")
+
+		end_search_found = end_search(end_morpion, end_search_trials)
+
+		end_searched[end_searchy.points_hash] = true
+
+		println("================================================")
+
+		c = 0
+		used = 0
+		for new_morpion in end_search_found
+
+			ph = points_hash(new_morpion)
+
+			# grows very rapidly is never cleared
+			#if score(new_morpion) >= score(end_morpion) + end_search_accept
+			#	end_searched[ph] = true
+			#end
+
+			# if score(new_morpion) >= score(end_morpion)
+			# 	end_searched[ph] = true
+			# end
+
+			if score(new_morpion) >= score(end_morpion) + index_accept &&
+				!haskey(index,ph) &&
+				!haskey(searched,ph)
+
+				#end_searched[ph] = true
+				used += 1
+				end_search_searchy = Searchy(new_morpion)
+
+				index[ph] = end_search_searchy
+
+				indicator = "->"
+				gap = " "
+
+				if score(new_morpion) >= score(end_morpion)
+					indicator = "=>"
+					gap = ""
+				end
+
+				println("| $(c).  $(score(end_morpion)) $indicator $(score(new_morpion)) ($(length(index)))")
+
+				if score(new_morpion) > max_score
+					is_record = true
+				end
+			end
+
+			c += 1
+		end
+
+		println ("  found: $(length(end_search_found)), used: $(used)")
+
+		println("================================================")
+	end
+
+	if length(index) > max_index_length
+
+		println ("$step. && Cleaning Index: $(max_index_length) -> $(length(index))")
+
+		index_searchies = sort(map((x)->x[2], index), by=(x)->x.score)
+		empty!(index)
+
+		for s in index_searchies[(length(index_searchies) - floor(max_index_length*forget_percent_save)):length(index_searchies)]
+			#s.visits = 0
+			index[s.points_hash] = s
+		end
+
+		empty!(morpion_cache)
+
+		# noticing that a lot of duplication is happening because clearing th end_searched
+		# empty!(end_searched)
+
+		max_index_length += index_length_increment
+	end
+
+	#if step >= sync_min && (step % sync_interval == 1)
+	if is_record
+		@time sync(index, searched, min_visited_score, filename, isrestarting)
+	end
+
+	if is_new_equal
+		last_active_step = step
+	end
+
+	searchy.visits += 1
+	step += 1
+
+	focus += focus_modifier
+
+	time_inactivity = step - last_active_step
+
+	if (time_inactivity > focus_inactive_reset && focus > focus_min_reset)
+
+		focus = focus_min
+
+		println("**************************************************************")
+		println("**************************************************************")
+
+		last_active_step = step
+	end
+end
