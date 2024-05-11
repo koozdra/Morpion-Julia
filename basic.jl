11c43b7ec0796406b55bf12d05bd3217ee62cbf8
Dimitri Tishchenko <koozdra@gmail.com>
2024-05-10 17:33:09 -0700

    more pushy

--- basic.jl
+++ basic.jl
@@ -239,7 +239,7 @@ function main()
 				num_time_steps_no_new_generated_counter += 1
 
 				# If we have reached the idle number of time steps that it makes sense to drop back
-				if num_time_steps_no_new_generated_counter >= 2
+				if num_time_steps_no_new_generated_counter >= 5
 					step_back += 1
 
 					empty!(index)
