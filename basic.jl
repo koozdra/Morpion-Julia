14fb0ad40bc5f4d363853ac876ef3e06365c0506
Dimitri Tishchenko <koozdra@gmail.com>
2024-06-04 06:28:25 -0700

    no end search resest

--- basic.jl
+++ basic.jl
@@ -230,7 +230,7 @@ function main()
 				num_time_steps_no_new_generated_counter += 1
 
 				# If we have reached the idle number of time steps that it makes sense to drop back
-				if num_time_steps_no_new_generated_counter >= 4
+				if num_time_steps_no_new_generated_counter >= 10
 					step_back += 1
 
 					candidates = []
@@ -308,9 +308,9 @@ function main()
 
 			# If no new configurations are found
 			# don't end search again
-			if !found_new
-				end_searched[index_key] = true
-			end
+			# if !found_new
+			end_searched[index_key] = true
+			# end
 		end
 
 		if improvement_counter >= 1
