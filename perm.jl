f66ce02091e56b9b7ac54dc871d69c9d107915ea
Dimitri Tishchenko <koozdra@gmail.com>
2025-08-19 14:55:20 -0700

    tune

--- perm.jl
+++ perm.jl
@@ -125,7 +125,7 @@ function main()
 
       key_score =
         if p_score >= (max_score - step_back)
-          p_score - (p_visits / 100000.0)
+          p_score - (p_visits / 200000.0)
         else
           0
         end
