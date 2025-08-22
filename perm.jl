b0edd4b3953b9a4d88ae383da88a544bd5cd8111
Dimitri Tishchenko <koozdra@gmail.com>
2025-08-22 06:48:31 -0700

    focus

--- perm.jl
+++ perm.jl
@@ -102,7 +102,7 @@ function main()
   inactivity_counter = 0
   inactivity_counter_reset = 1000000
   inactivity_new_found_counter = 0
-  inactivity_new_found_reset = 100
+  inactivity_new_found_reset = 10
   step_back = 0
 
   debug_interval = 100000
