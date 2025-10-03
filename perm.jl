832e89db2f00220cc7d37d5baf968089aa7ff8d0
Dimitri Tishchenko <koozdra@gmail.com>
2025-10-03 14:33:52 -0700

    tune

--- perm.jl
+++ perm.jl
@@ -100,7 +100,7 @@ function main()
   iteration = 0
 
   inactivity_counter = 0
-  inactivity_counter_reset = 1000000
+  inactivity_counter_reset = 500000
   inactivity_new_found_counter = 0
   inactivity_new_found_reset = 10
   step_back = 0
