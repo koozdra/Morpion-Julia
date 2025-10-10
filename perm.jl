1b15966f63a960d2031ca381926067e6c1487e31
Dimitri Tishchenko <koozdra@gmail.com>
2025-10-10 12:25:44 -0700

    tune

--- perm.jl
+++ perm.jl
@@ -157,7 +157,8 @@ function main()
 
     focus =
     # if iteration % 100 <= focus_balance
-      if iteration % 2 == 0
+    # if iteration % 2 == 0
+      if iteration % 10 == 0
         focus_min
       else
         focus_max
