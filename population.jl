d8ce1f1232d71eb90c64e5ca09264dec3c6dd362
Dimitri Tishchenko <koozdra@gmail.com>
2026-08-14 21:05:02 -0700

    tune

--- population.jl
+++ population.jl
@@ -118,7 +118,7 @@ function main()
   move_selection_skew = 1
 
   idle_reset = 10
-  idle_reset_step_back = 20
+  idle_reset_step_back = 10
   improvement_step_up = 100
 
   step_back_index_prune_size = 300_000
