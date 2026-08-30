b231b42f79e770804d7c598ee2ae02eede7e4ab5
Dimitri Tishchenko <koozdra@gmail.com>
2026-08-30 11:31:24 -0700

    tune

--- population.jl
+++ population.jl
@@ -117,13 +117,13 @@ function main()
   # 4 best
   # 5 too low
   # back_accept = 4
-  default_back_accept = 2
+  default_back_accept = 1
   # 2 best, 4 good, testing something higher, 10
   selection_skew = 10
 
   move_selection_skew = 1
 
-  idle_reset = 4
+  idle_reset = 16
   idle_reset_step_back = default_back_accept
   improvement_step_up = 16
 
