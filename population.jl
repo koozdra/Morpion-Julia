b64c739fa07daafed6069e67a6f93b6b71f3efe4
Dimitri Tishchenko <koozdra@gmail.com>
2026-09-23 08:03:26 -0700

    tune

--- population.jl
+++ population.jl
@@ -163,9 +163,9 @@ function main(; max_iterations::Union{Nothing,Int}=nothing,
   default_back_accept::Int=1,
   selection_skew::Real=2,
   move_selection_skew::Real=1,
-  idle_reset::Int=8,
+  idle_reset::Int=32,
   idle_reset_step_back::Int=default_back_accept,
-  improvement_step_up::Int=32,
+  improvement_step_up::Int=10,
   initial_candidates_size::Int=1)
   perm_length = 46 * 46 * 4
 
