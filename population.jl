7f2cd54dd0f455625a35e9352e6ae323b0d641bd
Dimitri Tishchenko <koozdra@gmail.com>
2026-09-23 17:28:19 -0700

    tune

--- population.jl
+++ population.jl
@@ -161,11 +161,11 @@ function main(; max_iterations::Union{Nothing,Int}=nothing,
   # search harness that explores them)
   num_modifications::Int=10,
   default_back_accept::Int=10,
-  selection_skew::Real=10,
+  selection_skew::Real=20,
   move_selection_skew::Real=1,
-  idle_reset::Int=64,
+  idle_reset::Int=128,
   idle_reset_step_back::Int=default_back_accept,
-  improvement_step_up::Int=64,
+  improvement_step_up::Int=128,
   initial_candidates_size::Int=1)
   perm_length = 46 * 46 * 4
 
